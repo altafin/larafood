b3a510d89b19dd0450aab75319eddbfb602dc2fe
altafin <marbr72@hotmail.com>
2021-08-25 15:10:00 -0300

    Módulo 04 - Aula 05

--- resources/views/admin/pages/profiles/_partials/form.blade.php
+++ resources/views/admin/pages/profiles/_partials/form.blade.php
@@ -1,5 +1,5 @@
-@csrf
 @include('admin.includes.alerts')
+@csrf
 <div class="form-group">
     <label>* Nome:</label>
     <input type="text" name="name" class="form-control" placeholder="Nome:" value="{{ $profile->name ?? old('name')}}">
