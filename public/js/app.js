3a096064c80e69f6da84b76e8b110453cc631c04
altafin <marbr72@hotmail.com>
2021-08-26 20:32:25 -0300

    Módulo 06 - Aula 01

--- public/js/app.js
+++ public/js/app.js
@@ -19432,8 +19432,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\laragon\www\larafood\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\laragon\www\larafood\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\laragon\www\larafood\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\laragon\www\larafood\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
