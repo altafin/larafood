43c62fdc033a5e1a7f676803d4ca29e71773d972
marcelo <altafin@uel.br>
2022-02-17 17:31:28 -0300

    Módulo 18 - Aula 02

--- app/Models/Traits/UserACLTrait.php
+++ app/Models/Traits/UserACLTrait.php
@@ -2,11 +2,30 @@
 
 namespace App\Models\Traits;
 
+use App\Models\Tenant;
+
 trait UserACLTrait
 {
-    public function permissions()
+    public function permissions(): array
     {
-        $tenant = $this->tenant()->first();
+        $permissionsPlan = $this->permissionsPlan();
+        $permissionsRole = $this->permissionsRole();
+
+        $permissions = [];
+        foreach ($permissionsRole as $permissionRole) {
+            if (in_array($permissionRole, $permissionsPlan)) {
+                array_push($permissions, $permissionsPlan);
+            }
+        }
+
+        return $permissions;
+    }
+
+    public function permissionsPlan(): array
+    {
+        // $tenant = $this->tenant()->first();
+        // $plan = $tenant->plan;
+        $tenant = Tenant::with('plan.profiles.permissions')->where('id', $this->tenant_id)->first();
         $plan = $tenant->plan;
 
         $permissions = [];
@@ -14,6 +33,17 @@ trait UserACLTrait
             foreach ($profile->permissions as $permission) {
                 array_push($permissions, $permission->name);
             }
+        }
+        return $permissions;
+    }
+
+    public function permissionsRole(): array
+    {
+        $roles = $this->roles()->with('permissions')->get();
+
+        $permissions = [];
+        foreach ($roles->permissions as $permission) {
+            array_push($permissions, $permission->name);
         }
         return $permissions;
     }
