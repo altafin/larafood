b2f512dddc833e93022dc0b91f00f1c9809866f9
marcelo <altafin@uel.br>
2022-02-17 17:51:58 -0300

    Módulo 18 - Aula 03

---
+++ app/Tenant/Events/TenantCreated.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Tenant\Events;
+
+use App\Models\{
+    Tenant,
+    User,
+};
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class TenantCreated
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    private $user;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Return user created.
+     *
+     * @return User
+     */
+    public function user(): User
+    {
+        return $this->user;
+    }
+
+    /**
+     * Get tenant.
+     */
+    public function tenant(): Tenant
+    {
+        return $this->user->tenant;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
