0d1a5695c63b47e85423d3152bd355c505d66301
marcelo <altafin@uel.br>
2022-02-18 10:33:06 -0300

    Módulo 19 - Aula 06

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/tenants/{uuid}', 'Api\TenantApiController@show');
 Route::get('/tenants', 'Api\TenantApiController@index');
