caa9f60cf44134d5b932049ea6279149d764a515
marcelo <altafin@uel.br>
2022-02-24 15:23:04 -0300

    Módulo 23 - Aula 02

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,7 @@ Route::prefix('admin')
     /**
      * Routes Tables
      */
+    Route::get('tables/qcode/{identify}', 'TableController@qrcode')->name('tables.qrcode');
     Route::any('tables/search', 'TableController@search')->name('tables.search');
     Route::resource('tables', 'TableController');
 
