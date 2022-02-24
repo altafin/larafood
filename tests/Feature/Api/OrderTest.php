242ea3f335255833e85797a6466d81f3d06b6e66
marcelo <altafin@uel.br>
2022-02-24 12:25:24 -0300

    Módulo 22 - Aula 13

--- tests/Feature/Api/OrderTest.php
+++ tests/Feature/Api/OrderTest.php
@@ -192,7 +192,6 @@ class OrderTest extends TestCase
         $response = $this->getJson('/api/auth/v1/my-orders', [
             'Authorization' => "Bearer {$token}",
         ]);
-        $response->dump();
 
         $response->assertStatus(200)
             ->assertJsonCount(2, 'data');
